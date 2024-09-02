68e9fd113278e9324c670ea9bdcf4c92941fa2a9
J-UENO\junya.ueno <junya.ueno@outlook.jp>
2024-09-02 14:32:32 +0900

    Player(バー)の基本操作の機能作成

---
+++ Assets/ColorBreakOut/Scripts/Player/BarController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ColorBreakOut
+{
+     /// <summary>
+    /// Playerが動かすBarの挙動を操作するクラス
+    /// </summary>
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class BarController : MonoBehaviour
+    {
+        /// <summary>バーが動くスピード</summary>
+        [SerializeField] float m_speed;
+        /// <summary>ボールのオブジェクト</summary>
+        [SerializeField] GameObject m_ball;
+
+        Rigidbody2D m_rb2d;
+
+        private void Start()
+        {
+            m_rb2d = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            // 水平方向の入力を検知する
+            float h = Input.GetAxisRaw("Horizontal");
+            // 入力に応じてBarを水平に動かす
+            m_rb2d.velocity = h * Vector2.right * m_speed;
+        }
+
+        // /// <summary>
+        // /// SpaceKeyが押されるまで最初のボールの射出を待つコルーチン
+        // /// </summary>
+        // IEnumerator BallPush()
+        // {
+        //     yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        //     m_ballList[0].gameObject.SetActive(true);
+        // }
+
+        // protected override void OnEnable()
+        // {
+        //     StartCoroutine(BallPush());
+        // }
+    }
+}
